56960c4a06ddecf341ea859ac383c91c9117755a
SametOzturk <53067521+SametOzturk61@users.noreply.github.com>
2020-02-05 12:08:38 +0300

    Create site.sql
    -Database added utf8_general_ci

---
+++ site.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 05 Şub 2020, 09:45:47
+-- Sunucu sürümü: 5.6.24
+-- PHP Sürümü: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Veritabanı: `site`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(255) NOT NULL,
+  `accountid` int(255) NOT NULL,
+  `videoid` int(255) NOT NULL,
+  `comment` text NOT NULL,
+  `date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(255) NOT NULL,
+  `regdate` varchar(255) NOT NULL,
+  `lastlogin` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `avatar` varchar(255) NOT NULL DEFAULT '/img/avatar/default.png',
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `ban` int(11) NOT NULL DEFAULT '0' COMMENT '0 = Normal, 1 = Banned'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `videos`
+--
+
+CREATE TABLE IF NOT EXISTS `videos` (
+  `id` varchar(255) NOT NULL,
+  `videoname` varchar(60) NOT NULL,
+  `videodesc` text NOT NULL,
+  `videocategory` varchar(255) NOT NULL,
+  `videothumbnail` varchar(255) NOT NULL,
+  `videosrc` varchar(255) NOT NULL,
+  `videouploaddate` varchar(255) NOT NULL,
+  `videoviews` varchar(255) NOT NULL DEFAULT '1',
+  `videoowner` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- Tablo için AUTO_INCREMENT değeri `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
